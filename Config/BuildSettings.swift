e666721e55735b1123410cf4490ef19bd8a1b416
Imam Hermawan <imam@aptika.co.id>
2026-03-02 09:05:44 +0700

    fix: update roomSettingsScreenShowAddressSettings to false for improved user interface consistency in BatChat

--- Config/BuildSettings.swift
+++ Config/BuildSettings.swift
@@ -367,7 +367,7 @@ final class BuildSettings: NSObject {
     static let roomSettingsScreenShowDirectChatOption: Bool = true
     static let roomSettingsScreenAllowChangingAccessSettings: Bool = true
     static let roomSettingsScreenAllowChangingHistorySettings: Bool = true
-    static let roomSettingsScreenShowAddressSettings: Bool = true
+    static let roomSettingsScreenShowAddressSettings: Bool = false
     static let roomSettingsScreenShowAdvancedSettings: Bool = true
     static let roomSettingsScreenAdvancedShowEncryptToVerifiedOption: Bool = true
 
